6a94ca82c3d71d2260f25139b3d5fe3e3108bc0f
David Michael Barr <b@rr-dav.id.au>
2017-04-29 22:29:18 +0900

    [CFL] Third iteration of CDF training after rebasing
    Change-Id: I1a8bf3c84fed134a82973dea4d128c2b7d7f8268
    Signed-off-by: David Michael Barr <b@rr-dav.id.au>

--- av1/common/entropymode.c
+++ av1/common/entropymode.c
@@ -2548,9 +2548,9 @@ static const aom_cdf_prob
 
 #if CONFIG_CFL
 static const aom_cdf_prob default_cfl_alpha_cdf[CFL_ALPHABET_SIZE] = {
-  // CDF after 8 iterations of RD-optimal alpha choice
-  23024, 23823, 26598, 26987, 27461, 28928, 29786, 30026,
-  30744, 31086, 31326, 31984, 32365, 32489, 32630, 32768
+  // CDF after 9 iterations of RD-optimal alpha choice
+  22979, 23770, 26549, 26934, 27427, 28840, 29741, 29984,
+  30713, 31061, 31303, 31972, 32362, 32485, 32629, 32768
 };
 #endif
 
